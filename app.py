0ef2ed99d10fd797413fa5dd4c8b930d3a7ea14c
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2012-08-14 19:29:33 +0530

    Basic version
    A pseudo working version of the app. Throw everything into respective
    static and template folders. Fix templates. Make App.py shoot rainbows

--- app.py
+++ app.py
@@ -1,13 +1,100 @@
 import os
-from flask import Flask
+import torstats
+from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+class Opt(object):
+    def __init__(self):
+        self.by_as = False
+        self.family = None
+        self.by_country = False
+        self.ases = None
+        self.country = None
+        self.top = 10
+        self.short = None 
+        self.links = None 
+        self.guards_only = None
+        self.inactive = False 
+        self.almost_fast_exits_only = None 
+        self.exits_only = False
+        self.download = None 
+        self.fast_exits_only = False
 
+class Result():
+    def __init__(self):
+        self.cw = None
+        self.adv_bw = None
+        self.p_guard = None
+        self.p_exit = None
+        self.p_middle = None
+        self.nick = None
+        self.fp = None
+        self.link = None
+        self.exit = None
+        self.guard = None
+        self.cc = None
+        self.as_no = None
+        self.as_name = None
+
+def parse(output_string):
+    results = []
+    for id, line in enumerate(output_string):
+        # skip headings
+        if id == 0: continue
+
+        result = Result()
+        values = line.split()
+
+        # TODO: change inaccurate value of 10
+        if len(values) > 10:
+            result.cw = values[0]
+            result.adv_bw = values[1]
+            result.p_guard = values[2]
+            result.p_middle = values[3]
+            result.p_exit = values[4]
+            result.nick = values[5]
+            result.fp = values[6]
+            result.exit = values[7]
+            result.guard = values[8]
+            result.cc = values[9]
+            result.as_no = values[10]
+            result.as_name = ' '.join(values[11:])
+            results.append(result)
+    return results
+
+@app.route('/')
+def index():    
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+    
+    options = Opt()
+    
+    for key, value in request.form.items():
+        print key, value
+        if key == "top":
+            options.top = int(value)
+        else:
+            setattr(options, key, value)
+
+    stats = torstats.RelayStats(options)
+    sorted_groups = stats.format_and_sort_groups(stats.relays,
+                        by_country=options.by_country,
+                        by_as_number=options.by_as,
+                        links=options.links)
+    output_string = stats.print_groups(sorted_groups, options.top,
+                   by_country=options.by_country,
+                   by_as_number=options.by_as,
+                   short=None,
+                   links=None)
+
+    return render_template('result.html', results=parse(output_string))
+    
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
